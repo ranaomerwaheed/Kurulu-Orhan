2206be6aa41c12ed17fb7eed314fb7dcbb594e2c
MUHAMMAD UMAR WAHEED <omerrana1422@outlook.com>
2025-09-30 00:54:31 +0500

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div className="text-center py-10">
+      <img
+        src="https://i.postimg.cc/qR1pTgGb/orhan-Banar-Main.png"
+        alt="Kurulus Orhan Banner"
+        className="mx-auto mb-6 rounded-xl shadow-lg"
+      />
+      <h1 className="text-3xl font-bold mb-4">Welcome to Kuruluş Orhan</h1>
+      <p className="text-lg">
+        Watch Kuruluş Orhan Episodes and Trailers directly here.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
