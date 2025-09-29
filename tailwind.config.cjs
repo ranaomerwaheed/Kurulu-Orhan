08d249dabcd0fd2a1a7674673fc6e608bb08a075
MUHAMMAD UMAR WAHEED <omerrana1422@outlook.com>
2025-09-30 00:48:22 +0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,14 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nastaleeq: ["JameelNooriNastaleeq", "serif"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
