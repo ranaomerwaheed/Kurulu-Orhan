61335d335e942244ffac3f925e7de0f509b99e28
MUHAMMAD UMAR WAHEED <omerrana1422@outlook.com>
2025-09-30 00:40:01 +0500

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,11 @@
-module.exports = {
-content: ['./index.html', './src/**/*.{js,jsx}'],
-theme: { extend: {} },
-plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
